002216f18edf60ac2afc374925d6162f09ed0bb2
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-16 19:43:21 +0700

    Setup redux

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { createStackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
+import { store, persistor } from "./src/store/store";
 import Login from "./src/pages/Login";
 
 const MainRoute = createStackNavigator({
   Login
 });
 
-const App = () => <MainRoute />;
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <MainRoute />
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import * as reducers from "./reducers";
+
+const persistConfig = {
+  storage,
+  key: "root",
+  whitelist: ["userSession"]
+};
+
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
